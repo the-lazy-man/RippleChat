f660aa659b90203b87790e2df609202c4e219dab
the-lazy-man <ankittiwari28032000@gmail.com>
2025-11-24 15:32:11 +0530

    upload method using signed preset done

--- app/src/main/java/com/example/ripplechat/app/ApplicationClass.kt
+++ app/src/main/java/com/example/ripplechat/app/ApplicationClass.kt
@@ -10,8 +10,8 @@ class RippleChatApp : Application(){
     override fun onCreate() {
         super.onCreate()
         val config: HashMap<String, String> = HashMap()
-        config["cloud_name"] = "dlk1m3v4m"   // from Cloudinary dashboard
-        config["api_key"] = "874437187174874"         // from Cloudinary dashboard
+        config["cloud_name"] = "dek45lsyv"   // from Cloudinary dashboard
+        config["api_key"] = "715399471187795"         // from Cloudinary dashboard
         config["api_secret"] = "hfvKhEsSiCGCf9bTB1NIdzdd95Q"   // ⚠️ only for dev/testing, backend signing recommended
 
         MediaManager.init(this,config)
